fadb4660efe6630fb51c367c8aaa0cbc2a6acbc3
Uriziel <urizieli@gmail.com>
2016-08-03 15:50:11 +0200

    crypto: Added crypto example

--- imports/modules/crypto/lib/module.js
+++ imports/modules/crypto/lib/module.js
@@ -1,4 +1,57 @@
 export const moduleName = "crypto";
-import {init, SetModule} from "angular2-now";
+import {init, SetModule, Directive, Inject, Filter} from "angular2-now";
+import CryptoJS from "crypto-js";
+
 init();
-SetModule(moduleName,[]);
+SetModule(moduleName, []);
+
+var KEY_SIZE = 256,
+    KEY_ITERATIONS = 10;
+
+export function initKey(password, salt) {
+    return CryptoJS.PBKDF2(password, salt, {keySize: KEY_SIZE / 32, iterations: KEY_ITERATIONS}).toString();
+}
+
+@Directive({
+    selector: 'encrypted-model',
+    bind: {key: '=encryptedModel'}
+})
+@Inject('@ngModel')
+class EncryptedModel {
+    constructor() {
+        this.$dependson = function (ngModel) {
+            var that = this;
+            ngModel.$formatters.push(function (value) {
+                if (value && that.key) {
+                    return CryptoJS.AES.decrypt(value, that.key)
+                        .toString(CryptoJS.enc.Utf8);
+                }
+
+                return value;
+            });
+
+            ngModel.$parsers.push(function (value) {
+                if (value && that.key) {
+                    return CryptoJS.AES.encrypt(value, that.key);
+                }
+
+                return value;
+            });
+        }
+
+    }
+}
+
+@Filter({name: 'decrypt'})
+class DecryptFilter {
+    constructor() {
+        return function (value, key) {
+            if (value && key) {
+                return CryptoJS.AES.decrypt(value, key)
+                    .toString(CryptoJS.enc.Utf8);
+            }
+
+            return value;
+        };
+    }
+}
