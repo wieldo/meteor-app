e143a2fc80482788feb1d079c036c2aad832ea67
Diabeu <scibor.rudnicki@ster-project.pl>
2016-08-09 17:34:32 +0200

    feat: add abstract clients state

---
+++ imports/startup/client/app/clients/index.js
@@ -0,0 +1,63 @@
+
+import clientsNavigationCollection from "./../../../../navigation/api/clients";
+import appNavigationCollection from "./../../../../navigation/api/app";
+import templateUrl from "./../view/signed-in.html";
+import {ModuleName} from "./../../";
+import {$Service} from "./../../../../ui/navigation/lib/service";
+import { SetModule, init, Component, State, LocalInjectables, MeteorReactive } from "angular2-now";
+
+init();
+SetModule(ModuleName);
+@State({
+    name: "app.clients",
+    abstract: true,
+    resolve: {
+        user: ($stateParams, $state, $timeout) => {
+            if (!Meteor.userId()) {
+                $timeout(function() {
+                    $state.go("app.signin");
+                },0);
+            }
+        }
+    }
+})
+@Component({
+    selector: "clients",
+    templateUrl: templateUrl,
+    providers: [
+        "$q",
+        "$state",
+        "SidenavService",
+        "$timeout",
+        "SimpleNavigationService"
+    ]
+})
+@LocalInjectables
+@MeteorReactive
+export class AppClientsComponent {
+    type = "clients";
+    constructor(){
+        this.sidenav = this.SidenavService.Init();
+        this.navigation_workspaces = clientsNavigationCollection;
+        this.navigation_app = appNavigationCollection;
+        this.SimpleNavigationService.getSelectedPromise().then(null, null, (notify) => {
+            this.selected = notify[this.type].selected.name;
+        });
+        this._helpers();
+    }
+
+    _helpers = () => {
+        this.helpers({
+            currentUser: () => {
+                this.currentUser = Meteor.user();
+                return Meteor.user();
+            }
+        });
+    }
+
+    logout = () => {
+        Meteor.logout((result) => {
+            this.$state.go("app.signin");
+        });
+    }
+}
