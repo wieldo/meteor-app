564a0efe5eb86ef73a5375c407f4247b496013ae
Diabeu <scibor.rudnicki@ster-project.pl>
2016-08-09 17:42:05 +0200

    refactor(forms): remove forms

--- imports/modules/forms/input/lib/client/helpers/my-own-function.js
+++
@@ -1,8 +0,0 @@
-export default (value, options, scope, event) => {
-    console.log("myOwnFunction");
-    console.log(value);
-    console.log(options);
-    console.log(scope);
-    console.log(event);
-    scope.to.label = "You have clicked on me, mmmm";
-};
--- imports/modules/forms/input/lib/client/wrapper/wrapper.js
+++
@@ -1,7 +0,0 @@
-import templateUrl from "./wrapper.html";
-export default (formlyConfigProvider) => {
-    formlyConfigProvider.setWrapper({
-        name: "customTemplateWrapper",
-        templateUrl: templateUrl
-    });
-};
