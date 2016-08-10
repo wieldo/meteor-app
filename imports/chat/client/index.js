b70bbeaa2e0b6fa5f4f30c948f7328a7f59a103f
Diabeu <scibor.rudnicki@ster-project.pl>
2016-08-10 18:38:02 +0200

    refactor(chat): change chat location folder

---
+++ imports/chat/client/index.js
@@ -0,0 +1,44 @@
+import "./style";
+import templateUrl from "./view";
+import {name as moduleName} from "./../../simple-chat/";
+import {init, SetModule, Component, State, LocalInjectables} from "angular2-now";
+
+init();
+SetModule(moduleName);
+@State({
+    name: "app.clients.chat",
+    url: "/chat",
+    resolve: {
+        user: ($stateParams, $state, $timeout) => {
+            if (!Meteor.userId()) {
+                $timeout(function() {
+                    $state.go("app.signin");
+                },0);
+            }
+            else
+                return Meteor.user();
+        }
+    }
+})
+@Component({
+    selector: "simple-chat",
+    templateUrl: templateUrl,
+    providers: [
+        "simpleChat",
+        "$timeout"
+    ]
+})
+@LocalInjectables
+export class ChatComponent {
+    constructor() {
+        console.log(this.simpleChat);
+    }
+    selectUser(userId) {
+        this.userId = userId;
+        this.channelId = undefined;
+    }
+    selectChannel(channelId) {
+        this.channelId = channelId;
+        this.userId = undefined;
+    }
+}
