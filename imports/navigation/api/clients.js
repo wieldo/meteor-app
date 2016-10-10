3c72bac5d4ada958ec747d84253072f784e7da31
Diabeu <scibor.rudnicki@ster-project.pl>
2016-10-10 17:10:46 +0200

    feature: clients and developers to create menu

--- imports/navigation/api/clients.js
+++ imports/navigation/api/clients.js
@@ -1,11 +1,16 @@
-export default {
-    header: "Menu",
-    name: "clients",
-    options: [
-        {name: "Dashboard",uisref:"app.clients.dashboard"},
-        {name: "Calendar",uisref:"app.clients.calendar", ngDisabled: true},
-        {name: "To do",uisref:"app.clients.todo"},
-        {name: "Settings",uisref:"app.clients.settings"},
-        {name: "Users",uisref:"app.clients.users", ngDisabled: true}
-    ]
+export default (providers) => {
+    return {
+        //header: "Menu",
+        name: "clients",
+        options: [
+            {icon: "zmdi zmdi-plus", name: "Create", ngclick: (ev) => {
+                providers.TodoFormService.mdDialog(ev);
+            }, select: false},
+            {icon: "zmdi zmdi-home", name: "Dashboard",uisref:"app.clients.dashboard"},
+            {icon: "zmdi zmdi-calendar", name: "Calendar",uisref:"app.clients.calendar", hidden: true},
+            {icon: "zmdi zmdi-check", name: "To do",uisref:"app.clients.todo"},
+            {icon: "zmdi zmdi-settings", name: "Settings",uisref:"app.clients.settings"},
+            {icon: "", name: "Users",uisref:"app.clients.users", hidden: true}
+        ]
+    };
 };
--- imports/navigation/api/developers.js
+++ imports/navigation/api/developers.js
@@ -1,11 +1,11 @@
 export default {
-    header: "Menu",
     name: "developers",
     options: [
-        {name: "Dashboard",uisref:"app.developers.dashboard"},
-        {name: "Crypto",uisref:"app.developers.crypto"},
-        {name: "Forms",uisref:"app.developers.forms"},
-        {name: "Translates",uisref:"app.developers.translates", ngDisabled: true},
-        {name: "UI Elements",uisref:"app.developers.uielements", hidden: true}
+        {icon: "zmdi zmdi-home", name: "Dashboard",uisref:"app.developers.dashboard"},
+        {name: "Auto formly",uisref:"app.developers.autoformly", hidden: true},
+        {icon: "zmdi zmdi-lock-outline", name: "Crypto",uisref:"app.developers.crypto"},
+        {name: "Navigation",uisref:"app.developers.navigation", hidden: true},
+        {icon: "zmdi zmdi-caret-right", name: "Forms",uisref:"app.developers.forms"},
+        {name: "Translates",uisref:"app.developers.translates", hidden: true}
     ]
 };
