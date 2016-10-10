c9924a777fc7c8b4f685b0176fa0cf3946b86bb2
Diabeu <scibor.rudnicki@ster-project.pl>
2016-10-10 19:05:34 +0200

    refactor(lib): simplify lib, Core, Routing modules only

--- imports/lib/index.js
+++ imports/lib/index.js
@@ -1,15 +1,6 @@
-
 import {ModuleName as Core} from "./core";
 import {moduleName as Routing} from "./routing";
-
-import hljs from "highlight.js";
-import jsfy from "jsfy";
-import highlight from "./helpers/highlight";
-
 export default {
     Core,
-    Routing,
-    highlight,
-    hljs,
-    jsfy
+    Routing
 };
