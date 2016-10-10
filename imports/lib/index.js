2746c73d829c81849067b513a67e81a61d1c93bf
Diabeu <scibor.rudnicki@ster-project.pl>
2016-10-10 17:09:18 +0200

    refactor(lib): move core library

---
+++ imports/lib/index.js
@@ -0,0 +1,15 @@
+
+import {ModuleName as Core} from "./core";
+import {moduleName as Routing} from "./routing";
+
+import hljs from "highlight.js";
+import jsfy from "jsfy";
+import highlight from "./helpers/highlight";
+
+export default {
+    Core,
+    Routing,
+    highlight,
+    hljs,
+    jsfy
+};
