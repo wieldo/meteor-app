134ba3e6dc3db1a2c684a80edc194365fa23be91
Diabeu <scibor.rudnicki@ster-project.pl>
2016-10-10 17:47:46 +0200

    refactor(startup/helpers): move

--- imports/startup/client/helpers/highlight.js
+++
@@ -1,9 +0,0 @@
-import hljs from "highlight.js";
-import $ from "jquery";
-export default (element) => {
-    $(document).ready(function() {
-        $(element).each(function(i, block) {
-            hljs.highlightBlock(block);
-        });
-    });
-};
