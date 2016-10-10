35979cd8fa31d0c38deaa01f82dd65970f19ce8c
Diabeu <scibor.rudnicki@ster-project.pl>
2016-10-10 17:36:17 +0200

    refactor(ui/navigation): change the way of using SimpleNavigationService, change bind, add component variable

--- imports/ui/navigation/client/index.js
+++ imports/ui/navigation/client/index.js
@@ -9,28 +9,39 @@ import {
 } from "angular2-now";
 import "./style";
 import templateUrl from "./view";
-import {$Service as SimpleNavigationService} from "./../lib/service";
+import {service} from "./../lib/service";
 init();
-export var $Component = {
+export var component = {
     name: "simple-navigation"
 };
 SetModule(moduleName);
 @Component({
-    selector: $Component.name,
+    selector: component.name,
     templateUrl: templateUrl,
     bind: {
-        options: "@",
-        name: "@",
-        header: "@"
+        data: "=?",
+        name: "=?",
+        options: "=?",
+        header: "=?",
+        parent: "=?"
     },
     providers: [
-        SimpleNavigationService.name
+        service.name
     ]
 })
 @LocalInjectables
 export class NavigationComponent {
     constructor() {
-        this.options = JSON.parse(this.options);
-        this.navigation = this.SimpleNavigationService.set(this.name, this.options).setHeader(this.header).get(this.name);
+        // If using data
+        if (this.data) {
+            let navigationService = this.SimpleNavigationService;
+            navigationService.set(this.data.name, this.data.options, this.data.header, this.data.parentName);
+            this.navigation = navigationService.get();
+        }
+        if (this.name && this.options) {
+            let navigationService = this.SimpleNavigationService;
+            navigationService.set(this.name, this.options, this.header, this.parent);
+            this.navigation = navigationService.get();
+        }
     }
 }
