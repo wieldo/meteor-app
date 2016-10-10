e661b11135b1f4b69b9e35a39a2934943c3645c2
Diabeu <scibor.rudnicki@ster-project.pl>
2016-10-10 17:23:56 +0200

    refactor(clients/settings): change state from signin to sign.in, change view

--- imports/clients/modules/settings/client/index.js
+++ imports/clients/modules/settings/client/index.js
@@ -11,7 +11,7 @@ SetModule(moduleName);
         user: ($stateParams, $state, $timeout) => {
             if (!Meteor.userId()) {
                 $timeout(function() {
-                    $state.go('app.signin');
+                    $state.go("app.sign.in");
                 },0);
             }
             else
