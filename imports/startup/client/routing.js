b77ac5ed01762e152dd1cd5cfc509a4c6260fb9a
Diabeu <scibor.rudnicki@ster-project.pl>
2016-10-10 17:48:17 +0200

    refactor(startup/client): move to lib

--- imports/startup/client/routing.js
+++
@@ -1,8 +0,0 @@
-import uiRouter from "angular-ui-router";
-import { init, SetModule } from "angular2-now";
-
-init();
-export const moduleName = "Routing";
-SetModule(moduleName,[
-    uiRouter
-]);
