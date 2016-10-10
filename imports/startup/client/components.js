a12f17b7b22142f89c8dcc0664c6988f92d5f534
Diabeu <scibor.rudnicki@ster-project.pl>
2016-10-10 17:49:14 +0200

    feature(startup): import all components, import all modules

---
+++ imports/startup/client/components.js
@@ -0,0 +1,8 @@
+// Dashboard Component
+import "./../../clients/modules/dashboard/client/";
+// Signin Component
+import "./../../sign/in/client/";
+// Signup Component
+import "./../../sign/up/client/";
+// Settings Component
+import "./../../clients/modules/settings/client/";
---
+++ imports/startup/client/modules.js
@@ -0,0 +1,14 @@
+// Dashboard Module
+import "./../../clients/modules/dashboard/lib/module";
+// Developers modules
+import "./../../developers/modules/index";
+// To do Module
+import "./../../clients/modules/todo/lib/";
+// Signin Module
+import "./../../sign/in/lib/module";
+// Signup Module
+import "./../../sign/up/lib/module";
+// Settings Module
+import "./../../clients/modules/settings/lib/module";
+// Navigation Module
+import "./../../ui/navigation/lib/module";
