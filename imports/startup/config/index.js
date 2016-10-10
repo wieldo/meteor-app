37c50155d7482e47128d4bbd7bc03019ca6f3230
Diabeu <scibor.rudnicki@ster-project.pl>
2016-10-10 18:00:59 +0200

    feature(config): set index with config imports, set md-theming-provider, sce-provider, simple-schema-messages, state-provider, location-provider

---
+++ imports/startup/config/index.js
@@ -0,0 +1,10 @@
+import mdThemingProvider from "./../config/md-theming-provider";
+import stateProvider from "./../config/state-provider";
+import locationProvider from "./../config/location-provider";
+import sceProvider from "./../config/sce-provider";
+export default {
+    mdThemingProvider,
+    stateProvider,
+    locationProvider,
+    sceProvider
+};
---
+++ imports/startup/config/sce-provider.js
@@ -0,0 +1,3 @@
+export default ($sceProvider) => {
+    $sceProvider.enabled(false);
+};
