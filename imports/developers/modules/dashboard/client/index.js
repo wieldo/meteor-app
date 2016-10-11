10d39c0dc523cffd6d951025a1d24564043898b1
Diabeu <scibor.rudnicki@ster-project.pl>
2016-10-11 14:44:18 +0200

    refactor(DevelopersDashboardComponent): add some styles

--- imports/developers/modules/dashboard/client/index.js
+++ imports/developers/modules/dashboard/client/index.js
@@ -1,3 +1,4 @@
+import "./style";
 import templateUrl from "./view";
 import {moduleName} from "./../lib/module";
 import {init,SetModule,Component, State} from "angular2-now";
@@ -6,11 +7,11 @@ SetModule(moduleName);
 @State({
     name: "app.developers.dashboard",
     url: "/dashboard"
-})
+});
 @Component({
     selector: "developers-dashboard",
     templateUrl: templateUrl
-})
+});
 export class DevelopersDashboardComponent {
     constructor() {
 
