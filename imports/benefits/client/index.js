a44f87559634e767ed680660da59f085015ea3a9
Diabeu <scibor.rudnicki@ster-project.pl>
2016-10-11 15:03:52 +0200

    refactor(BenefitsComponent): add exported component variable

--- imports/benefits/client/index.js
+++ imports/benefits/client/index.js
@@ -1,15 +1,17 @@
-import templateUrl from "./view";
 const benefitsTemplateUrl = templateUrl;
+import templateUrl from "./view";
 export default benefitsTemplateUrl;
 import {ModuleName} from "./../../startup/client/";
 import {init, SetModule, Component} from "angular2-now";
-init();
 
-SetModule(ModuleName);
-@Component({
+export var component = {
+    name: "BenefitsComponent",
     selector: "benefits",
     templateUrl: templateUrl
-});
+};
+init();
+SetModule(ModuleName);
+@Component(component)
 export class BenefitsComponent {
     constructor(){
         this.test = "miaaau";
