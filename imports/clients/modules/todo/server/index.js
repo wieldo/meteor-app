349a8172d67fe11876fde566e40ed703f80a863c
Diabeu <scibor.rudnicki@ster-project.pl>
2016-10-11 15:26:44 +0200

    refactor(TodoModule): server publish from publications, index as imports
    Create index to import importants

---
+++ imports/clients/modules/todo/server/index.js
@@ -0,0 +1,4 @@
+import "./fixtures";
+import "./publish";
+import "./../api/allow";
+import "./../api/methods";
---
+++ imports/clients/modules/todo/server/publish.js
@@ -0,0 +1,38 @@
+import {todoCollection} from "./../api/collection";
+
+Meteor.startup(function(){
+    this.debug = false;
+    if (Meteor.isServer){
+        Meteor.publish("todo", (selector, options) => {
+            let fields = {
+                name: 1,
+                createdBy: 1,
+                removed: 1,
+                completed: 1,
+                createdAt: 1
+            };
+            if (!options) {
+                options = {
+                    fields: {
+                        name: 1,
+                        createdBy: 1,
+                        removed: 1,
+                        completed: 1,
+                        createdAt: 1
+                    },
+                    sort: {
+                        createdAt: -1
+                    }
+                };
+            } else {
+                options.fields = fields;
+            }
+            if (this.debug === true) {
+                console.log(`Meteor.publish("todo", (selector, options) => {})`);
+                console.log(`selector`, selector);
+                console.log(`options`, options);
+            }
+            return todoCollection.find(selector, options);
+        });
+    }
+});
