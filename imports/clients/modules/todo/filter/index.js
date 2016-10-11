0099646cfeaea8a7971479246c05575d776a52c6
Diabeu <scibor.rudnicki@ster-project.pl>
2016-10-11 16:53:00 +0200

    feature(todoFilter): add index to import and export todos filters

---
+++ imports/clients/modules/todo/filter/index.js
@@ -0,0 +1,6 @@
+import {TodoFilterCreatedByComponent} from "./created-by/client";
+import {TodoFilterStatusComponent} from "./status/client";
+export default {
+    TodoFilterCreatedByComponent,
+    TodoFilterStatusComponent
+};
