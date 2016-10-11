24bc3416b080427927a1bfc48f3c15b9aea077e4
Diabeu <scibor.rudnicki@ster-project.pl>
2016-10-11 14:42:41 +0200

    refactor(CryptoComponent): import style, change component selector, add form variable to component and view

--- imports/developers/modules/crypto/client/index.js
+++ imports/developers/modules/crypto/client/index.js
@@ -1,4 +1,5 @@
-import templateUrl from "./view.html";
+import templateUrl from "./view";
+import "./style";
 import crypto from "./../lib/crypto";
 import {moduleName} from "./../lib/module";
 import {init, SetModule, Component, State, LocalInjectables} from "angular2-now";
@@ -8,12 +9,12 @@ SetModule(moduleName);
 @State({
     name: "app.developers.crypto",
     url: "/crypto"
-})
+});
 @Component({
-    selector: "crypto1",
+    selector: "crypto",
     templateUrl: templateUrl
-})
-@LocalInjectables
+});
+@LocalInjectables;
 export class CryptoComponent {
     /* Salt should be backend-generated and stored data */
     salt = "135576d158c5ed15132ffc36decde569fe59fef78b6192cd3a521205fb43c47344";
@@ -21,6 +22,7 @@ export class CryptoComponent {
     password = "MyPASSWORD_1235!@#";
     fields = {};
     model = {};
+    form = "crypto";
     constructor() {
         this.key = crypto().initKey(this.password, this.salt);
         this.myModel = "Example text to encrypt";
@@ -57,40 +59,46 @@ export class CryptoComponent {
                 }
             },
             {
-                type: "input",
-                key: "valuetoencrypt",
+                "input",
+                    key: "valuetoencrypt",
                 templateOptions: {
-                    label: "Value",
-                    description: "Specify value to encrypt",
+                    "Value",
+                        description: "Specify value to encrypt",
                     onKeyup: (options, value, scope, event) => {
                         let encrypted = crypto().encrypt(scope.model.valuetoencrypt, scope.model.key);
                         scope.model.encryptedvalue = encrypted.toString();
                     }
-                }
-            },
+                };;;
+            };;,
             {
-                type: "input",
-                key: "encryptedvalue",
+                "input",
+                    key: "encryptedvalue",
                 templateOptions: {
-                    label: "Encrypted value",
-                    readonly: true,
+                    "Encrypted value",
+                        readonly: true,
                     description: "Encrypted value from input above and settings above"
-                }
-            },
+                };;
+            };;,
             {
-                type: "input",
-                key: "decryptedvalue",
+                "input",
+                    key: "decryptedvalue",
                 templateOptions: {
-                    label: "Decrypted value",
-                    readonly: true,
+                    "Decrypted value",
+                        readonly: true,
                     description: "Decrypted value from input above and settings above"
-                },
-                expressionProperties: {
-                    "model": ($viewValue, $modelValue, scope) => {
+                };;,
+                {
+                    "model"
+                :
+                    ($viewValue, $modelValue, scope) =
+                >
+                    {
                         scope.model.decryptedvalue = crypto().decrypt(scope.model.encryptedvalue, scope.model.key);
                     }
                 }
-            }
-        ];
+                ;
+                ;
+            };;
+    ]
     }
 }
