7cb2e789a798dd976d7f43e9e866f28660c4e93c
Diabeu <scibor.rudnicki@ster-project.pl>
2016-10-11 16:46:52 +0200

    refactor(FormsComponent): remove import components

--- imports/developers/modules/forms/client/index.js
+++ imports/developers/modules/forms/client/index.js
@@ -1,44 +1,33 @@
-import templateUrl from "./view";
 import "./style";
+import templateUrl from "./view";
 import {ModuleName} from "./../lib/module";
-// Forms input examples
-import "./../input/client/";
-// Forms checkbox examples
-import "./../checkbox/client/";
-// Forms chips examples
-import "./../chips/client/";
-// Forms datepicker examples
-import "./../datepicker/client/";
-// Forms radio examples
-import "./../radio/client/";
-// Forms select examples
-import "./../select/client/";
-// Forms select birthday examples
-import "./../select/birthday/client/";
-// Forms slider examples
-import "./../slider/client/";
-// Forms switch examples
-import "./../switch/client/";
-// Forms textarea examples
-import "./../textarea/client/";
 import formsNavigationCollection from "./../api/navigation";
-import {init,SetModule, State, Component, LocalInjectables} from "angular2-now";
-init();
-SetModule(ModuleName);
-@State({
+import {
+    init,
+    SetModule,
+    State,
+    Component,
+    LocalInjectables
+} from "angular2-now";
+export var state = {
     name: "app.developers.forms",
     url: "/forms"
-})
-@Component({
+};
+export var component = {
+    name: "FormsComponent",
+    state: state,
     selector: "meteor-forms",
     templateUrl: templateUrl,
     providers: [
         "$mdSidenav"
     ]
-})
+};
+init();
+SetModule(ModuleName);
+@State(state)
+@Component(component)
 @LocalInjectables
 export class FormsComponent {
     navigation = formsNavigationCollection;
-    constructor(){
-    }
+    constructor(){}
 }
