f4dfa35ea12f3566594e237167f8b917dcc12df3
Diabeu <scibor.rudnicki@ster-project.pl>
2016-10-11 15:08:04 +0200

    refactor(DevelopersDashboardComponent): add exported state and component var

--- imports/developers/modules/dashboard/client/index.js
+++ imports/developers/modules/dashboard/client/index.js
@@ -1,19 +1,27 @@
 import "./style";
 import templateUrl from "./view";
 import {moduleName} from "./../lib/module";
-import {init,SetModule,Component, State} from "angular2-now";
-init();
-SetModule(moduleName);
-@State({
+import {
+    init,
+    SetModule,
+    Component,
+    State
+} from "angular2-now";
+export var state = {
     name: "app.developers.dashboard",
     url: "/dashboard"
-});
-@Component({
+};
+export var component = {
+    name: "DevelopersDashboardComponent",
+    state: state,
     selector: "developers-dashboard",
     templateUrl: templateUrl
-});
+};
+init();
+SetModule(moduleName);
+@State(state)
+@Component()
 export class DevelopersDashboardComponent {
     constructor() {
-
     }
 }
