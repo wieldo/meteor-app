d70cd5ebbfd1c076845d068659992973705a3fd1
Diabeu <scibor.rudnicki@ster-project.pl>
2016-10-19 16:02:25 +0200

    docs(ChatComponent): change name to chat

--- imports/chat/client/index.js
+++ imports/chat/client/index.js
@@ -1,8 +1,7 @@
 import "./style";
 import templateUrl from "./view";
-import {name as moduleName} from "./../../simple-chat/";
-import {init, SetModule, Component, State, LocalInjectables} from "angular2-now";
-
+import { name as moduleName } from "./../../simple-chat/";
+import { init, SetModule, Component, State, LocalInjectables } from "angular2-now";
 init();
 SetModule(moduleName);
 @State({
@@ -15,29 +14,43 @@ SetModule(moduleName);
                     $state.go("app.sign.in");
                 },0);
             }
-            else
+            else {
                 return Meteor.user();
+            }
         }
     }
 })
 @Component({
-    selector: "simple-chat",
+    selector: "chat",
     templateUrl: templateUrl,
     providers: [
-        "simpleChat",
         "$timeout"
     ]
 })
 @LocalInjectables
 export class ChatComponent {
+    debug = true;
+
     constructor() {
         console.log(this.simpleChat);
     }
-    selectUser(userId) {
+
+    /**
+     * [selectUser description]
+     * @param  {[type]} userId [description]
+     * @return {[type]}        [description]
+     */
+    selectUser = (userId) => {
         this.userId = userId;
         this.channelId = undefined;
     }
-    selectChannel(channelId) {
+
+    /**
+     * [selectChannel description]
+     * @param  {[type]} channelId [description]
+     * @return {[type]}           [description]
+     */
+    selectChannel = (channelId) => {
         this.channelId = channelId;
         this.userId = undefined;
     }
