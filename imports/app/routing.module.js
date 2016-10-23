603be8d9303f5e46228c4602d58f1a3e92264aa6
Diabeu <scibor.rudnicki@ster-project.pl>
2016-10-23 21:32:44 +0200

    refactor(Routing): change name to RoutingModule

--- imports/app/routing.module.js
+++ imports/app/routing.module.js
@@ -1,7 +1,7 @@
 import uiRouter from "angular-ui-router";
 import { init, SetModule } from "angular2-now";
 init();
-export const moduleName = "Routing";
+export const moduleName = "RoutingModule";
 export const RoutingModule = SetModule(moduleName,[
     uiRouter
 ]);
